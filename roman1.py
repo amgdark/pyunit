261ed3efdbd17288d7f42a7e62eaed6dac40a504
amgdark <amgdark@uaz.edu.mx>
2014-09-09 15:02:31 -0500

    Update roman1.py
    Se agregó el método from_roman, y la clase NoIntegerError, para manejar la excepción lanzada por números flotantes

--- roman1.py
+++ roman1.py
@@ -1,11 +1,5 @@
-"""
->>> to_roman(4000)
-'MMMM'
->>> to_roman(5000)
-'MMMMM'
->>> to_roman(9000)
-'MMMMMMMMM'
-"""
+# -*- coding: utf8 -*-
+
 roman_numeral_map = (('M',  1000),
                      ('CM', 900),
                      ('D',  500),
@@ -23,17 +17,32 @@ roman_numeral_map = (('M',  1000),
 
 def to_roman(n):
     '''convert integer to Roman numeral'''
-    if not (0< n < 4000):
+    if not (0 < n < 4000):
         raise OutOfRangeError('numero fuera del rango (1-3999)')
-
+    if not isinstance(n, int):
+        raise NoIntegerError('Sólo numeros enteros')
     result = ''
     for numeral, integer in roman_numeral_map:
         while n >= integer:
-            result+= numeral
+            result += numeral
             n -= integer
     return result
 
+def from_roman(s):
+    '''convert Roman numeral to integer'''
+    result = 0
+    index = 0
+    for numeral, integer in roman_numeral_map:
+        while s[index:index+len(numeral)] == numeral:
+            result += integer
+            index += len(numeral)
+    return result
+
 class OutOfRangeError(ValueError):
+    pass
+
+
+class NoIntegerError(ValueError):
     pass
 
 if __name__ == '__main__':
