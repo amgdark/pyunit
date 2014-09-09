1e5fae9b3a61801bd85c59a30432d469e93cd4b9
amgdark <amgdark@uaz.edu.mx>
2014-09-09 15:01:17 -0500

    Update romantest2.py
    Se agregaron nuevos casos de prueba, para los métodos to_roman, y from_roman

--- romantest2.py
+++ romantest2.py
@@ -1,6 +1,6 @@
 import unittest
 import roman1
-
+from romantest1 import KnownValues as kv
 
 class ToRomanBadInput(unittest.TestCase):
 
@@ -15,6 +15,20 @@ class ToRomanBadInput(unittest.TestCase):
 	def test_negative(self):
 		'''to_roman should fail with negative input'''
 		self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, -1)
+	def test_no_integer(self):
+		'''to_roman should fail with float input '''
+		self.assertRaises(roman1.NoIntegerError,roman1.to_roman,0.5)
+	def test_from_roman_known_values(self):
+		'''from_roman should give known result with known input'''
+		for integer, numeral in kv.known_values:
+			result = roman1.from_roman(numeral)
+			self.assertEqual(integer, result)	
+	def test_roundtrip(self):
+		'''from_roman(to_roman(n))==n for all n'''
+		for integer in range(1, 4000):
+			numeral = roman1.to_roman(integer)
+			result = roman1.from_roman(numeral)
+			self.assertEqual(integer, result)
 
 if __name__ == '__main__':
 	unittest.main()
