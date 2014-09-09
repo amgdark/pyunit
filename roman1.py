e4cba9e1be99e46b03099620ac7f7f4583fff43a
amgdark <root@alexDebian>
2014-09-08 21:18:16 -0500

    Se agregarón las pruebas con excepciones para datos erroneos
    Se agregó el archivo romantest2.py con nuevas pruebas,
    se agregó al archivo roma1.py la clase OutOfRange para la
    excepción lanzada por el test.

---
+++ roman1.py
@@ -0,0 +1,41 @@
+"""
+>>> to_roman(4000)
+'MMMM'
+>>> to_roman(5000)
+'MMMMM'
+>>> to_roman(9000)
+'MMMMMMMMM'
+"""
+roman_numeral_map = (('M',  1000),
+                     ('CM', 900),
+                     ('D',  500),
+                     ('CD', 400),
+                     ('C',  100),
+                     ('XC', 90),
+                     ('L',  50),
+                     ('XL', 40),
+                     ('X',  10),
+                     ('IX', 9),
+                     ('V',  5),
+                     ('IV', 4),
+                     ('I',  1))
+
+
+def to_roman(n):
+    '''convert integer to Roman numeral'''
+    if n > 3999:
+        raise OutOfRangeError('numero fuera del rango (1-3999)')
+
+    result = ''
+    for numeral, integer in roman_numeral_map:
+        while n >= integer:
+            result+= numeral
+            n -= integer
+    return result
+
+class OutOfRangeError(ValueError):
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ romantest2.py
@@ -0,0 +1,9 @@
+import unittest, roman1
+
+class ToRomanBadInput(unittest.TestCase):
+	def test_too_large(self):
+		'''to_roman shoul fail with large input'''
+		self.assertRaises(roman1.OutOfRangeError, roman1.to_roman,4000)
+
+if __name__ == '__main__':
+	unittest.main()
