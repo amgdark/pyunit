32f1a312fb9767811334a06ac0e6b29339cc3fac
amgdark <amgdark@uaz.edu.mx>
2014-09-08 21:41:01 -0500

    Update romantest2.py

--- romantest2.py
+++ romantest2.py
@@ -1,9 +1,20 @@
-import unittest, roman1
+import unittest
+import roman1
+
 
 class ToRomanBadInput(unittest.TestCase):
+
 	def test_too_large(self):
 		'''to_roman shoul fail with large input'''
-		self.assertRaises(roman1.OutOfRangeError, roman1.to_roman,4000)
+		self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, 4000)
+
+	def test_zero(self):
+		''' to_roman should fail with 0 input '''
+		self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, 0)
+
+	def test_negative(self):
+		'''to_roman should fail with negative input'''
+		self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, -1)
 
 if __name__ == '__main__':
 	unittest.main()
